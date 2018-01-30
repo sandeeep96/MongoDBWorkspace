71ff6f373c7ba2e764d85922c43817e870508fbf
sandeeep96 <saisandeepdvn.96@gmail.com>
2018-01-30 16:43:33 +0530

    added the new url and made corresponding changes

--- Angular/src/app/mydata.service.ts
+++ Angular/src/app/mydata.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import { Observable }     from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -10,13 +10,19 @@ export class MyDataService {
 
     constructor(private http: Http) { }
 
-    private endpointURL = 'http://localhost:3000/winners';  // URL to web api
+    // private endpointURL = 'http://localhost:3000/winners';  // URL to web api
+    private endpointURL = 'http://172.24.214.51:8085/solr/report/select?q=*:*';
 
-    private headers = new Headers({'Content-Type': 'application/json'});
+    private headers = new Headers({'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'});
+
+    private options = new RequestOptions({ headers: this.headers });
 
     getWinners(): Observable<MyData[]> {
         return this.http.get(this.endpointURL)
-                   .map(response => response.json() as MyData[])
+                   .map(response => {
+                     console.log(response);
+                     console.log(response.json().response.docs);
+                     return response.json().response.docs as MyData[]})
                    .catch(this.handleError);
       }
        
